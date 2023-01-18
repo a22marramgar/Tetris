019b31c61a6117363e2ec5ca526820a8906afdbf
ausias <ausias@hINFO6-28.xiam.cat>
2023-01-18 13:24:17 +0100

    Colores añadidos (no puedo naranja)

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -3,12 +3,48 @@ package tetris;
 import java.util.Scanner;
 
 public class Main {
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
 
 	public static void MostrarTaulell(int Taulell[][]) {
 		// bucle imprimir la matriz
 		for (int i = 0; i < Taulell.length; i++) {
 			for (int j = 0; j < Taulell[0].length; j++) {
-				System.out.print(Taulell[i][j] + " ");
+				switch(Taulell[i][j]) {
+					case 0:
+						System.out.print("□");
+						break;
+					case 1:
+						System.out.print(ANSI_CYAN+"■");
+						break;
+					case 2:
+						System.out.print(ANSI_YELLOW+"■");
+						break;
+					case 3:
+						System.out.print(ANSI_GREEN+"■");
+						break;
+					case 4:
+						System.out.print(ANSI_BLUE+"■");
+						break;
+					case 5:
+						System.out.print(ANSI_WHITE+"■");
+						break;
+					case 6:
+						System.out.print(ANSI_RED+"■");
+						break;
+					case 7:
+						System.out.print(ANSI_PURPLE+"■");
+						break;
+				}
+				System.out.print(ANSI_RESET);
 			}
 			System.out.println();
 		}
