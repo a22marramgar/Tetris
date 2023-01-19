371aff364a82488bc5177a3bdf30b7c91e7ee2e4
ausias <ausias@hINFO6-28.xiam.cat>
2023-01-19 10:02:02 +0100

    Cositas

--- src/tetris/ControlPieza.java
+++ src/tetris/ControlPieza.java
@@ -14,7 +14,7 @@ public class ControlPieza {
 		case 2:// PiezaO
 			Pieza[3][0] = 2;
 			Pieza[3][1] = 2;
-			Pieza[2][0] = 2; 
+			Pieza[2][0] = 2;
 			Pieza[2][1] = 2;
 			break;
 		case 3:// PiezaS
@@ -60,53 +60,88 @@ public class ControlPieza {
 		while (!lanzar) {
 			Main.MostrarTaulell(Pieza);
 			System.out.println("a: moure esquerra, d: moure dreta, s: llençar peça");
-			switch(scan.nextLine().toLowerCase()) {
-				case "a":
-					Pieza = MoureEsquerra(Pieza);
-					break;
-				case "d":
-					Pieza = MoureDreta(Pieza);
-					break;
-				case "s":
-					lanzar = true;
-					break;
-				default:
-					break;
+			switch (scan.nextLine().toLowerCase()) {
+			case "a":
+				Pieza = MoureEsquerra(Pieza);
+				break;
+			case "d":
+				Pieza = MoureDreta(Pieza);
+				break;
+			case "s":
+				lanzar = true;
+				break;
+			default:
+				break;
 			}
 		}
 		return Pieza;
 	}
-	public static int[][] MoureEsquerra(int[][] Pieza){
+
+	public static int[][] MoureEsquerra(int[][] Pieza) {
 		boolean imposible = false;
-		for (int i = 0; i < Pieza[0].length&&!imposible; i++) {
-			for (int j = 0; j < Pieza.length&&!imposible; j++) {
-				if(Pieza[j][i]!=0&&i>0) {
-					Pieza[j][i-1] = Pieza[j][i];
+		for (int i = 0; i < Pieza[0].length && !imposible; i++) {
+			for (int j = 0; j < Pieza.length && !imposible; j++) {
+				if (Pieza[j][i] != 0 && i > 0) {
+					Pieza[j][i - 1] = Pieza[j][i];
 					Pieza[j][i] = 0;
-				}else if(Pieza[j][i]!=0&&i==0) {
+				} else if (Pieza[j][i] != 0 && i == 0) {
 					imposible = true;
 				}
 			}
 		}
 		return Pieza;
 	}
-	public static int[][] MoureDreta(int[][] Pieza){
+
+	public static int[][] MoureDreta(int[][] Pieza) {
 		boolean imposible = false;
-		for (int i = Pieza[0].length-1; i >= 0&&!imposible; i--) {
-			for (int j = 0; j <Pieza.length&&!imposible; j++) {
-				if(Pieza[j][i]!=0&&i<Pieza[0].length-1) {
-					Pieza[j][i+1] = Pieza[j][i];
+		for (int i = Pieza[0].length - 1; i >= 0 && !imposible; i--) {
+			for (int j = 0; j < Pieza.length && !imposible; j++) {
+				if (Pieza[j][i] != 0 && i < Pieza[0].length - 1) {
+					Pieza[j][i + 1] = Pieza[j][i];
 					Pieza[j][i] = 0;
-				}else if(Pieza[j][i]!=0&&i==Pieza[0].length-1) {
+				} else if (Pieza[j][i] != 0 && i == Pieza[0].length - 1) {
 					imposible = true;
 				}
 			}
 		}
 		return Pieza;
 	}
-	
-	public static int[][] FerCaureLaPieza(int[][] Pieza, int[][] Taulell){
-		
+
+	public static void ActualitzarPieza(int[][] Pieza) {
+		for (int i = 0; i < Pieza[0].length; i++) {
+			for (int j = 1; j < Pieza.length; j++) {
+				Pieza[Pieza.length - j][i] = Pieza[Pieza.length - (j + 1)][i];
+			}
+			Pieza[0][i] = 0;
+		}
+	}
+
+	public static void ActualitzarTaulell(int[][] Taulell) {
+		for (int j = Taulell.length - 2; j >= 0; j--) {
+			for (int k = 0; k < Taulell[0].length; k++) {
+				if (Taulell[j][k] != 0 && Taulell[j + 1][k] == 0) {
+					Taulell[j + 1][k] = Taulell[j][k];
+					Taulell[j][k] = 0;
+				}
+			}
+		}
+	}
+
+	public static int[][] FerCaureLaPieza(int[][] Pieza, int[][] Taulell) {
+		for (int i = 0; i < Pieza[0].length; i++) {
+			// Bajar Pieza al Taulell
+			if (Pieza[Pieza.length - 1][i] != 0 && Taulell[0][i] == 0) {
+				Taulell[0][i] = Pieza[Pieza.length - 1][i];
+			}
+
+		}
+		ActualitzarPieza(Pieza);
+		ActualitzarTaulell(Taulell);
+		for (int i = 0; i < Pieza[0].length; i++) {
+			if (Pieza[Pieza.length - 1][i] != 0) {
+				FerCaureLaPieza(Pieza, Taulell);
+			}
+		}
 		return Taulell;
 	}
 }
